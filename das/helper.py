a6fd66e931b9cd665e1e6d8987dced79370aa835
David Wibisono <davidwibisono95@gmail.com>
2019-06-16 22:49:16 +0700

    add generate thumbnail

--- das/helper.py
+++ das/helper.py
@@ -4,6 +4,12 @@ from validation import *
 from file_manager import upload
 import datetime
 from math import radians, cos, sin, asin, sqrt
+import os
+import PIL
+from PIL import Image
+import base64
+from io import BytesIO
+
 
 def strToTimedelta(time):
 	if "." in time:
@@ -43,7 +49,6 @@ def attach(doctype,name,filedata):
 	return response
 
 
-
 def haversine(lon1, lat1, lon2, lat2):
     """
     Calculate the great circle distance between two points 
@@ -59,3 +64,48 @@ def haversine(lon1, lat1, lon2, lat2):
     c = 2 * asin(sqrt(a)) 
     r = 6371 # Radius of earth in kilometers. Use 3956 for miles
     return c * r
+
+
+def generate_thumbnail(filename, doctype, name, field):
+	# generate thumbnail using Image PIL
+	sitename = cstr(frappe.local.site)
+	cd = os.getcwd()
+	img = Image.open(os.path.join(cd,sitename,'private','files',filename))
+	basewidth = 300
+	wpercent = (basewidth / float(img.size[0]))
+	hsize = int((float(img.size[1]) * float(wpercent)))
+	img = img.resize((basewidth, hsize), PIL.Image.ANTIALIAS)
+	buffered = BytesIO()
+	img.save(buffered,format="JPEG")
+	img_str = base64.b64encode(buffered.getvalue())
+
+	
+	#upload thumnail image
+	req = frappe.local.form_dict
+	req.filedata = img_str
+	req.name = name
+	req.filename = "thumbnail_{}".format(filename)
+	
+	response = {}
+	try:
+		uploaded = upload(doctype, req.name, 1)
+
+		response["code"] = 200
+		response["message"] = "Success"
+		response["data"] = uploaded
+
+		frappe.db.sql("UPDATE `tab{}` SET {}='{}' WHERE name='{}'".format(doctype, field, uploaded['file_url'], name))
+
+		frappe.db.commit()
+
+
+	except Exception as e:
+		response["code"] = 400
+		response["message"] = e.message
+		response["data"] = ""
+	except UnboundLocalError as e:
+		response["code"] = 401
+		response["message"] = e.message
+		response["data"] = ""
+
+	return response
